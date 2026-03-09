82a85edf7b0167f2abaf28933848bac49cd8bb2f
Tobydog0501 <toby@tobydog.org>
2026-03-09 12:49:23 +0800

    Clipboard feature

--- init.lua
+++ init.lua
@@ -1,2 +1,17 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.g.clipboard = {
+  name = "osc52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = function()
+      return { "" }
+    end,
+    ["*"] = function()
+      return { "" }
+    end,
+  },
+}
